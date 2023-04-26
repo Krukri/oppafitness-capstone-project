1a2e19143c1a13abdfad65ca4ea697bd857aa7ca
Krukri <josephrbanaag51@gmail.com>
2023-04-27 00:53:02 +0800

    update chat design

--- src/components/profileContents/chatWithCoach.js
+++ src/components/profileContents/chatWithCoach.js
@@ -144,7 +144,7 @@ const ChatWithCoach = () => {
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
-              fill="currentColor"
+              fill="#ffb703"
               class="bi bi-send-fill"
               viewBox="0 0 16 16"
             >
